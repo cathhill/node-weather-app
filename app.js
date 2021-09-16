5e7a46e457ac6f301c7fe675aada16c1dc3d6b3c
cathhill <86472730+cathhill@users.noreply.github.com>
2021-09-16 10:30:29 +0100

    created geocode callback

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
 const request = require("request");
+const geocode = require("./utils/geocode");
 
 /* const url =
   "http://api.weatherstack.com/current?access_key=b47ae6e976872c6c13ce18b07fb7564d&query=37.8267,-122.4233&units=m";
 
 //json:true parses the data.
 request({ url: url, json: true }, (error, response) => {
-  //   console.log(response.body.current);
-  console.log(
-    response.body.current.weather_descriptions[0] +
-      ". It is " +
-      response.body.current.temperature +
-      " degrees and feels like " +
-      response.body.current.feelslike +
-      " degrees."
-  );
+  if (error) {
+    console.log("Unable to connect to weather service!");
+  } else if (response.body.error) {
+    console.log("Unable to find location.");
+  } else {
+    console.log(
+      response.body.current.weather_descriptions[0] +
+        ". It is " +
+        response.body.current.temperature +
+        " degrees and feels like " +
+        response.body.current.feelslike +
+        " degrees."
+    );
+  }
 }); */
 
-const geocoding =
+/* const geocodeURL =
   "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY2F0aDIxIiwiYSI6ImNrdGtqaXFkMTAxdnYyb28zNWhkZHZ4cnYifQ.fOFq2ACp6bdmpqVipz3OSA&limit=1";
 
-request({ url: geocoding, json: true }, (error, response) => {
-  const latitude = response.body.features[0].center[1];
-  const longitude = response.body.features[0].center[0];
-  console.log("Latitude = " + latitude + " and longitude = " + longitude);
+request({ url: geocodeURL, json: true }, (error, response) => {
+  if (error) {
+    console.log("Unable to connect to location services");
+  } else if (response.body.features.length === 0) {
+    console.log("Unable to find location. Try another search.");
+  } else {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log("Latitude = " + latitude + " and longitude = " + longitude);
+  }
+}); */
+
+// callback often takes two arguments: error and data. Only one of these will have a value, the other will be undefined.
+geocode("Philadelphia", (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
 });

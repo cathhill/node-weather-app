e971ec73e107001531b76a6789d7c22b23b4e7f7
cathhill <86472730+cathhill@users.noreply.github.com>
2021-09-23 13:02:49 +0100

    Error handling

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-// callback often takes two arguments: error and data. Only one of these will have a value, the other will be undefined.
-geocode("Philadelphia", (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
+const address = process.argv[2];
 
-forecast(44.1545, -75.7088, (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
+// !address = if no address is given
+if (!address) {
+  console.log("Please provide an address");
+} else {
+  // callback often takes two arguments: error and data. Only one of these will have a value, the other will be undefined.
+  geocode(address, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+    //return only returns one thing and then stops running, so the forecast data won't show if there is a geocode error.
+
+    // forecastData comes from geocode function. Can't be called data as prevents getting 'data' from geocode above.
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(data.location);
+      console.log(forecastData);
+    });
+  });
+}
